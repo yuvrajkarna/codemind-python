75b9962c4b70a6a13b18c0dcc0dfee3ab1f123a6
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-06-28 13:21:32 +0530

    n=int(input()) l=list(map(int,input().split())) k=int(input()) ma=max(l) for i in range(n):     if l[i]+k>=ma:         print("True",end=" ")     else:         print("False",end=" ")

--- Greatest_Number_of_Candies.py
+++ Greatest_Number_of_Candies.py
@@ -1,9 +1,9 @@
 n=int(input())
-m=list(map(int,input().split()))
+l=list(map(int,input().split()))
 k=int(input())
-ma=max(m)
+ma=max(l)
 for i in range(n):
-    if m[i]+k>=ma:
+    if l[i]+k>=ma:
         print("True",end=" ")
     else:
         print("False",end=" ")
