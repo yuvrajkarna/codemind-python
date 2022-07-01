bed53f5a07d3ddb4bb481eb40a3e421c97248e68
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-07-01 15:53:43 +0530

    def lcm(a,b):     l=max(a,b)     while True:         if l%a==0 and l%b==0:             break         else:             l+=max(a,b)     return l a=int(input()) arr=list(map(int,input().split())) l=lcm(arr[0],arr[1]) for i in range(2,a):     l=lcm(l,arr[i]) print(l)

---
+++ LCM_of_n_numbers.py
@@ -0,0 +1,15 @@
+def lcm(a,b):
+    l=max(a,b)
+    while True:
+        if l%a==0 and l%b==0:
+            break
+        else:
+            l+=max(a,b)
+    return l
+a=int(input())
+arr=list(map(int,input().split()))
+l=lcm(arr[0],arr[1])
+for i in range(2,a):
+    l=lcm(l,arr[i])
+print(l)
+
