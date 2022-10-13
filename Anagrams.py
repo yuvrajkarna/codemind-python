77276ade95ebbd29283d7e85c397ae25ae2d9e1b
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-10-13 22:14:52 +0530

    a=input() b=input() f=0 if len(a)==len(b):     for i in a:         if i in b or i.upper() in b or i.lower() in b:             f=1         else:             f=0             break     if f==1:         print("True")     else:         print("False") else:     print("False")

---
+++ Anagrams.py
@@ -0,0 +1,16 @@
+a=input()
+b=input()
+f=0
+if len(a)==len(b):
+    for i in a:
+        if i in b or i.upper() in b or i.lower() in b:
+            f=1
+        else:
+            f=0
+            break
+    if f==1:
+        print("True")
+    else:
+        print("False")
+else:
+    print("False")
