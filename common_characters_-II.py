35fc390131f6691802af548b03cde5177422fb73
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-10-14 22:22:30 +0530

    a=input() b=input() arr=[] brr=[] for i in a:     if i.isspace():         continue     else:         arr.append(i.lower()) for i in b:     if i.isspace():         continue     else:         brr.append(i.lower()) rrr=[] for i in arr:     if i in brr:         if i not in rrr:             rrr.append(i) rrr.sort() print(len(rrr))

---
+++ common_characters_-II.py
@@ -0,0 +1,21 @@
+a=input()
+b=input()
+arr=[]
+brr=[]
+for i in a:
+    if i.isspace():
+        continue
+    else:
+        arr.append(i.lower())
+for i in b:
+    if i.isspace():
+        continue
+    else:
+        brr.append(i.lower())
+rrr=[]
+for i in arr:
+    if i in brr:
+        if i not in rrr:
+            rrr.append(i)
+rrr.sort()
+print(len(rrr))
