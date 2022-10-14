8da78d615dd3d79fc69826162905930b9aa09bdd
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2022-10-14 18:13:43 +0530

    a,b=map(int,input().split()) arr=list(map(int,input().split())) arr2=list(map(int,input().split())) for i in range(a):     for j in range(a):         if i!=j:             if arr[i]==arr[j] and arr[i]!=-1:                 arr[j]=-1 for i in range(b):     for j in range(b):         if i!=j:             if arr2[i]==arr2[j] and arr2[i]!=-1:                 arr2[j]=-1 for i in arr:     if i!=-1:         if i not in arr2:             print(i,end=" ") for i in arr2:     if i!=-1:         if i not in arr:             print(i,end=" ")

---
+++ Common_elements_II.py
@@ -0,0 +1,21 @@
+a,b=map(int,input().split())
+arr=list(map(int,input().split()))
+arr2=list(map(int,input().split()))
+for i in range(a):
+    for j in range(a):
+        if i!=j:
+            if arr[i]==arr[j] and arr[i]!=-1:
+                arr[j]=-1
+for i in range(b):
+    for j in range(b):
+        if i!=j:
+            if arr2[i]==arr2[j] and arr2[i]!=-1:
+                arr2[j]=-1
+for i in arr:
+    if i!=-1:
+        if i not in arr2:
+            print(i,end=" ")
+for i in arr2:
+    if i!=-1:
+        if i not in arr:
+            print(i,end=" ")
